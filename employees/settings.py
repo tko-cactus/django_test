5f3d9892e939ed1a92d252778d75e252e366a08f
tko-cactus <37374201+tko-cactus@users.noreply.github.com>
2021-06-03 15:30:56 +0900

    change db from postgres to sqlite

--- employees/settings.py
+++ employees/settings.py
@@ -88,8 +88,8 @@ WSGI_APPLICATION = "employees.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": BASE_DIR / "db.postgresql",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
